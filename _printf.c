daaa10a655a776234193e3c2c64b9e8a1a2db53a
rania3103 <raniaabassi33@gmail.com>
2023-07-28 09:18:47 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -19,7 +19,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (*format)
 	{
-		if(format[0] == '%' && !format[1])
+		if (format[0] == '%' && !format[1])
 			return (-1);
 		while (format[i])
 		{
