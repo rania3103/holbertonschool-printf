7babf430fc3064277f3d46af28ac684879f63b6c
rania3103 <raniaabassi33@gmail.com>
2023-07-28 09:42:23 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -16,18 +16,18 @@ int _printf(const char *format, ...)
 	{
 		return (-1);
 	}
-	if (format[0] == '%' && (!format[1] || format[1] == '\0'))
-		return (-1);
 	va_start(args, format);
 	if (*format)
 	{
 		while (format[i])
 		{
+			if (format[i] == '%' && (!format[i + 1] || format[i + 1] == '\0'))
+				return (-1);
 			if (format[i] == '%')
 			{
 				func_ptr = get_func(format[i + 1]);
-				if (format[i + 1] == '\0')
-					return (-1);
+				/*if (format[i + 1] == '\0')
+					return (-1);*/
 				if (func_ptr)
 				{
 					count += func_ptr(args);
